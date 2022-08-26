da0e82b2a963253d039865fc4ee72621a11ee2af
J-Preethi <112074123+J-Preethi@users.noreply.github.com>
2022-08-26 14:37:11 +0530

    first commit

---
+++ Data/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public delegate void MyDelagate(int x);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelagate obj = new MyDelagate(M1);
+            obj += M2;
+            obj.Invoke(10);
+            Console.ReadLine();
+        }
+        public static void M1(int x)
+        {
+            Console.WriteLine(x * x*x);
+        }
+
+        public static void M2(int x)
+        {
+            Console.WriteLine(x * x * x*x);
+        }
+    }
+}
+
+      
